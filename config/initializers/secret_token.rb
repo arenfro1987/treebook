b72dacc7389c0ae9866066a1a64ae0315c9623cf
Derek Renfro <drenfro87@gmail.com>
2013-01-11 12:45:45 -0600

    created trrbook with status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '263c3859bb856e41eb842985e20fd9295e3108e5d8392f227780b7cd83afdd00665bda5dfc22bb38c901a3978780048238aa639d1adf9f4791fdb9961b8ed612'
